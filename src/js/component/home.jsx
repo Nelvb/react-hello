5928dfd781bca54a402326ed25ec876b62c5ecd3
Nelvb <169047839+Nelvb@users.noreply.github.com>
2024-08-21 10:59:32 +0000

    Finalizados cambios

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Digit } from "./digit";
 
-//create your first component
 const Home = () => {
     const [timer, setTimer] = useState(0);
 
